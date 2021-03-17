7a927e1ef31d8787e2bc3a1bf7ba90eb09f05c86
Antonio Mindov <amindov98@gmail.com>
2021-03-17 15:22:25 +0200

    tmp

--- Assets/ThirdPersonController.cs
+++ Assets/ThirdPersonController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThirdPersonController : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform camera;
     public float speed;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -14,14 +15,16 @@ public class ThirdPersonController : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
         if (direction.magnitude > 0.1)
         {
-            controller.Move(direction * speed * Time.deltaTime);
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
     }
 }
